e00f0c966275deb6dd7a44174fcccec7252bbd72
jorgenwh <jorgenwhen@gmail.com>
2022-10-12 15:26:36 +0200

    init

---
+++ cupycounter/counter.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cupy as cp
+
+
+class Counter():
+    def __init__(self, keys, capacity: int = 0, capacity_factor: int = 1.75):
+        pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cupycounter",
+    version="0.0.1",
+    author="Jørgen Henriksen",
+    description="",
+    long_description="",
+    packages=find_packages("cupycounter"),
+    package_dir={"":"cupycounter"},
+    zip_safe=False,
+)
